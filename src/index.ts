6d19eb0907f0ccae87a194188ab54bd4469fb6d2
unknown <asarejerry16@gmail.com>
2024-04-17 12:05:23 +0000

    resolver for related data commit

--- src/index.ts
+++ src/index.ts
@@ -23,6 +23,25 @@ const resolvers = {
       return db.authors.find((author) => author.id === args.id);
     },
   },
+  Game: {
+    reviews(parent) {
+      // 'parent' here is the resolved Game object
+      return db.reviews.filter((review) => review.game_id === parent.id);
+    },
+  },
+  Author: {
+    reviews(parent) {
+      return db.reviews.filter((review) => review.author_id === parent.id);
+    },
+  },
+  Review: {
+    game(parent) {
+      return db.games.find((game) => game.id === parent.game_id);
+    },
+    author(parent) {
+      return db.authors.find((author) => author.id === parent.author_id);
+    },
+  },
 };
 
 // server setup
